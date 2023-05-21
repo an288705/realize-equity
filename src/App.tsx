5356c896d22aa6a7401f3582d314555b207c555b
an288705 <aj0466012@gmail.com>
2023-05-20 20:05:41 -0400

    implement react router and add simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 import Home from './Views/Home';
+import SignIn from './Views/SignIn';
+import SignUp from './Views/SignUp';
 import './App.css';
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/premium-themes/onepirate/">
+        <Route index element={<Home/>}/>
+        <Route path="/premium-themes/onepirate/sign-in/" element={<SignIn/>}/>
+        <Route path="/premium-themes/onepirate/sign-up/" element={<SignUp/>}/>
+      </Route>
+    )
+  );
+
   return (
-    <Home/>
+    <RouterProvider router={router} />
   );
 }
 
