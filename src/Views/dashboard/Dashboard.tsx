c6acb2e1890fff0678ea0dfee8c49c25c47189fa
an288705 <aj0466012@gmail.com>
2023-05-26 00:27:28 -0400

    add simple navbar

--- src/Views/dashboard/Dashboard.tsx
+++ src/Views/dashboard/Dashboard.tsx
@@ -1,10 +1,14 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../controllers/contexts';
+import Navbar from './Navbar';
 
 export default function DashBoard() {
     const user = useContext(UserContext);
 
     return (
-        <>Logged In: {user.email}</>
+        <>
+          <Navbar />
+          <>Logged In: {user.email}</>
+        </>
       );
 }
