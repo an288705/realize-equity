0c6a7619f779fd44adab55191df7d4ea77c207c6
an288705 <aj0466012@gmail.com>
2023-06-03 19:27:44 -0400

    use better chart library

--- src/Views/dashboard/AccountChart.tsx
+++ src/Views/dashboard/AccountChart.tsx
@@ -1,43 +1,72 @@
-import Paper from '@mui/material/Paper';
-import {
-  ArgumentAxis,
-  ValueAxis,
-  Chart,
-  LineSeries,
-} from '@devexpress/dx-react-chart-material-ui';
-import { ValueScale } from '@devexpress/dx-react-chart';
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-interface IDataItem {
-  month: string,
-  sale: number,
-  total: number,
-}
-
-const chartData: IDataItem[] = [
-  { month: 'Jan', sale: 50, total: 987 },
-  { month: 'Feb', sale: 100, total: 3000 },
-  { month: 'March', sale: 30, total: 1100 },
-  { month: 'April', sale: 107, total: 7100 },
-  { month: 'May', sale: 95, total: 4300 },
-  { month: 'June', sale: 150, total: 7500 },
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
 ];
 
-export default () => {
+export default function AccountChart() {
     return (
-      <Paper>
-        <Chart
-          data={chartData}
+        <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+            }}
         >
-          <ValueScale name="sale" />
-          <ValueScale name="total" />
-
-          <LineSeries
-            name="Total Transactions"
-            valueField="total"
-            argumentField="month"
-            scaleName="total"
-          />
-        </Chart>
-      </Paper>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
     );
-}
+  }
+
