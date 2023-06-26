32a46e2468b5bcbe074a60c05438ed2e135ce7b0
an288705 <aj0466012@gmail.com>
2023-06-25 23:37:19 -0700

    show account value

--- src/Views/dashboard/AccountChart.tsx
+++ src/Views/dashboard/AccountChart.tsx
@@ -1,5 +1,6 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { UserContext } from '../../controllers/contexts';
 
 const data = [
   {
@@ -47,26 +48,33 @@ const data = [
 ];
 
 export default function AccountChart() {
+    const user = React.useContext(UserContext);
+
     return (
-        <LineChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
+        <div>
+            <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+            <div>
+                Account val {user.cashBalance+user.sharesBalance}
+            </div>
+        </div>
     );
   }
 
